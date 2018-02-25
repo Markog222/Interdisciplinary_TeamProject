0328a1acd4103946f99608cd2c36fbeada7e1cfa
Stefan Dworschak <x15037835@student.ncirl.ie>
2018-02-25 17:15:58 +0000

    added youtube widget

--- features/google drive auth/app.py
+++ features/google drive auth/app.py
@@ -5,19 +5,26 @@ from flask.ext.session import Session
 
 
 
-app=Flask(__name__)
+app=Flask(__name__, static_url_path='/static')
 # Check Configuration section for more details
-SESSION_TYPE = 'redis'
-app.config.from_object(__name__)
-Session(app)
+#SESSION_TYPE = 'redis'
+#app.config.from_object(__name__)
+#Session(app)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
+@app.route('/gimages')
+def gimages():
+	return render_template('includes/gimages.html')
+	
 @app.route('/gslides')
 def gslides():
-	return render_template('gslides.html')
+	return render_template('includes/gslides.html')
 
+@app.route('/twitter')
+def twitter():
+	return render_template('includes/twitter.html')
 
 app.run(debug=True)
