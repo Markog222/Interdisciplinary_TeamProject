a60535071832ae27072796f2b87c184f2edae777
Paddy-NCirl17 <p.jmoran@icloud.com>
2018-04-03 19:19:51 +0100

    Add files via upload
    Login, 
    Register with authentication
    Change password

--- Kanvass App/app.py
+++ Kanvass App/app.py
@@ -1,16 +1,36 @@
 #Python session: http://pythonhosted.org/Flask-Session/
 from flask import Flask, render_template, url_for, request, session, redirect, flash
 from flask_pymongo import PyMongo
+from flask_oauth import OAuth
 import bcrypt
 import json
+import re
 
 app=Flask(__name__, static_url_path='/static')
-
+#app.debug = DEBUG
 app.config['MONGO_DBNAME'] = 'kanvasslgin'
 app.config['MONGO_URI'] = 'mongodb://kanvass:kanvasslogin@ds247058.mlab.com:47058/kanvasslgin'
+#GOOGLE_CLIENT_ID = '799140526216-ba5ib3ejqkq0k02fa9nk2debtijt72t4.apps.googleusercontent.com'
+#GOOGLE_CLIENT_SECRET = 'snO9Vfar1RqqR6FBZkzC386q'
+#REDIRECT_URI = '/oauth2callback'
+
+#SECRET_KEY = 'development key'
+#DEBUG = True
 
 mongo = PyMongo(app)
+#oauth = OAuth()
 
+#google = oauth.remote_app('google',
+#                          base_url='https://www.google.com/accounts/',
+#                          authorize_url='https://accounts.google.com/o/oauth2/auth',
+#                          request_token_url=None,
+#                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+#                                                'response_type': 'code'},
+#                          access_token_url='https://accounts.google.com/o/oauth2/token',
+#                          access_token_method='POST',
+#                          access_token_params={'grant_type': 'authorization_code'},
+#                          consumer_key=GOOGLE_CLIENT_ID,
+#                          consumer_secret=GOOGLE_CLIENT_SECRET)
 #
 # Login and Registration
 # @reference: https://www.youtube.com/watch?v=vVx1737auSE
@@ -26,20 +46,7 @@ def kid():
 def index():
     if 'username' in session:
         new_id=kid()
-        kans=getAllKans()
-        shared=allSharedKans()
-        d={}
-        d['k_id']=new_id
-        kan_list=[]
-        shared_list=[]
-        for kan in kans:
-            kan_list.append(kan['kid'])
-        d['kans']=kan_list
-        for share in shared:
-            shared_list.append(share['kid'])
-        d['shared']=shared_list
-        dJson=json.dumps(d)
-        return render_template('home.html',data=dJson)
+        return render_template('home.html',k_id=new_id)
 
     return render_template('index.html')
 
@@ -48,33 +55,82 @@ def register():
     if request.method == 'POST':
         users = mongo.db.users
         existing_user = users.find_one({'name' : request.form['username']})
-
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'name' : request.form['username'], 'password' : hashpass})
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-
-        return 'That username already exists!'
+        pass1 = request.form['pass']#requesting first password from registration form
+        pass2 = request.form['pass1']#requesting second password fromm registration form
+        
+        if pass1 == pass2:#Checks the passwords are the same else return error
+            if len(pass1)>=6 and re.match('^(?=.*[0-9]$)(?=.*[a-zA-Z])',pass1): 
+                if existing_user is None:
+                    hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+                    users.insert({'name' : request.form['username'],'fullname' : request.form['fullname'], 'password' : hashpass})
+                    #adding to database
+                    session['username'] = request.form['username']
+                    return redirect(url_for('index'))
+                else:
+                    flash('That username already exists!','category2')
+                    return redirect (url_for('register'))
+            else:
+                flash('Password must be six characters long and alphanumeric','category2')
+                return redirect (url_for('register'))            
+        else:
+            flash('The passwords do not match!','category2')
+            return redirect (url_for('register'))
 
     return render_template('register.html')
 
+@app.route('/reset', methods=['POST', 'GET'])
+def reset():
+    if request.method == 'POST':
+        users = mongo.db.users
+        find_p = session['username']
+        print (find_p)
+        find = mongo.db.users.find({'name':find_p})
+        for find_p in find:
+            store_pass = (find_p['password'])
+            store_user = (find_p['name'])
+            store_full = (find_p['fullname'])
+            store_id = (find_p['_id'])
+            print (store_pass,store_user,store_full,store_id)
+        pass1 = request.form['pass']#requesting first password from registration form
+        pass2 = request.form['pass1']#requesting first password from registration form
+        if pass1 == pass2:#Checks the passwords are the same else return error
+            if len(pass1)>=6 and re.match('^(?=.*[0-9]$)(?=.*[a-zA-Z])',pass1): 
+                if 'username' in session:
+                    hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+                    update =  mongo.db.users.update_one({'password' : store_pass},{'$set':{'password' : hashpass}},upsert=False)
+                    #replace = mongo.db.users.replace_one({'password' : store_pass},{'_id' : store_id},{'fullname' : store_full},{'password' : hashpass},{'name' : store_full})
+    
+                    return redirect(url_for('index'))
+                else:
+                    flash('That username does not exist!','category2')
+                    return redirect (url_for('reset'))
+            else:
+                flash('Password must be six characters long and alphanumeric','category2')
+                return redirect (url_for('reset'))            
+        else:
+            flash('The passwords do not match!','category2')
+            return redirect (url_for('reset'))
+
+    return render_template('reset.html')
+
 @app.route('/login', methods=['POST'])
 def login():
-    users = mongo.db.users
-    login_user = users.find_one({'name' : request.form['username']})
+        users = mongo.db.users
+        login_user = users.find_one({'name' : request.form['username']})#find username
 
-    if login_user:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
+        if login_user:
+            if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+                #if password equals database password login
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
 
-    return 'Invalid username/password combination'
+        flash('Invalid username/password combination','category2')
+        return redirect(url_for('index'))   
 
 @app.route('/logout')
 def logout():
     session.pop('username',None)
-    flash('You have logged out.')
+    flash('You have logged out.','category1')
     return redirect('/')
 
 @app.route('/create')
@@ -83,7 +139,6 @@ def create():
 		return render_template('create.html')
 
 	return render_template('index.html')
-
 
 @app.route('/save', methods=['POST'])
 def save():
@@ -95,69 +150,20 @@ def save():
 @app.route('/delWidget',methods=['POST'])
 def delWidget():
     data=json.loads(request.data)['delWidget']
+    print(wid)
     widgetsDb=mongo.db.widgets
     widgetsDb.delete_one({"kid":wid[0]['kid'],"kid":wid[0]['wid']})
 
     return True
-
-@app.route('/view')
-def view():
-
-    if 'username' in session:
-        k_id=request.args.get('k')
-        kDb=mongo.db.kanvasses
-        wDb=mongo.db.widgets
-        kanvass=kDb.find_one({'kid':k_id})
-        widgets=wDb.find({'kid':k_id})
-        d={}
-        kanvass.pop('_id',None)
-        d['kan']=kanvass
-        widget_list=[]
-        for item in widgets:
-            item.pop('_id',None)
-            widget_list.append(item)
-
-        d['widgets']=widget_list
-        d=json.dumps(d)
-
-        return render_template('view.html',data=d)
-
-    return render_template('index.html')
-
-@app.route('/edit')
-def edit():
-
-    if 'username' in session:
-        k_id=request.args.get('k')
-        kDb=mongo.db.kanvasses
-        wDb=mongo.db.widgets
-        kanvass=kDb.find_one({'kid':k_id})
-        widgets=wDb.find({'kid':k_id})
-        d={}
-        kanvass.pop('_id',None)
-        d['kan']=kanvass
-        widget_list=[]
-        for item in widgets:
-            item.pop('_id',None)
-            widget_list.append(item)
-
-        d['widgets']=widget_list
-        d=json.dumps(d)
-
-        return render_template('edit.html',data=d)
-
-    return render_template('index.html')
 
 # Update writing to DB with bulk_write for better performance later
 def saveKanvass(data):
     kanDB=mongo.db.kanvasses
     check=kanDB.count({"kid":data[0]['kid']})
     if check==0:
-        print(data)
         kanDB.insert({
             "kid":data[0]['kid'],
             "owner":session['username'],
-            "heading":data[0]['heading'],
             "shared":0,
             "shareid":0,
             "sharedwith":[],
@@ -165,9 +171,7 @@ def saveKanvass(data):
         })
     else:
         kanDB.find_one_and_replace({"kid":data[0]['kid']},{
-            "kid":data[0]['kid'],
             "owner":session['username'],
-            "heading":data[0]['heading'],
             "shared":0,
             "shareid":0,
             "sharedwith":[],
@@ -195,8 +199,6 @@ def insertWidgets(data):
         else:
             widDb.find_one_and_replace({"kid":widget['kid'],
             "wid":widget['wid']},{
-                "kid":widget['kid'],
-                "wid":widget['wid'],
                 "type":widget['type'],
                 "style":widget['style'],
                 "pos":widget['pos'],
@@ -205,16 +207,6 @@ def insertWidgets(data):
                 "link":widget['link']
             })
     return 'true'
-
-def getAllKans():
-    kDb=mongo.db.kanvasses
-    kans=kDb.find({"owner":session['username']})
-    return kans
-
-def allSharedKans():
-    kDB=mongo.db.kanvasses
-    shared=kDB.find({"sharedwith": session['username']})
-    return shared
 
 app.secret_key = 'mysecret'
 app.run(debug=True)
