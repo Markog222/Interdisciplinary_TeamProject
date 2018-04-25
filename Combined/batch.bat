24fcdf2b4c969a56fc6d673a6aa0510b5aa0f987
Tetsuya Matsushita <x15045111@student.ncirl.ie>
2018-04-25 13:11:35 +0100

    Delete batch.bat

--- Combined/batch.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd C:\Python34\Scripts\
-C:\Python34\Scripts\pip install flask
-C:\Python34\Scripts\pip install Flask-Session
-C:\Python34\Scripts\pip install httplib2
-C:\Python34\Scripts\pip install --upgrade google-api-python-client
-pause
-exit
